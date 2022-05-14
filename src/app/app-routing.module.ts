922fc693347e9d5b8a62ce76d98ee611d2625c9d
rtj-meanstack <rtj113@gmail.com>
2022-05-14 15:31:34 -0700

    Added the detail route with an :id so it routes to that specific object id number as a URL with data passing though correctly.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AzureBoardsComponent } from './core/azure-boards/azure-boards.component';
 import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { BoardDetailComponent } from './core/board-detail/board-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'boards', component: AzureBoardsComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: BoardDetailComponent }
 ];
 
 @NgModule({
